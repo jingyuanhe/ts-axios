18c944f791fdd53995e93d5f2509059ddf9a1061
jingyuanhe <389026847@qq.com>
2020-08-05 22:37:50 +0800

    处理请求url

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -4,6 +4,7 @@ const webapck = require('webpack');
 module.exports = {
     mode: 'development',
     entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
+        console.log(entries,dir);
         const fullDir = path.join(__dirname,dir);
         const entry = path.join(fullDir, 'app.ts');
         if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import {AxiosReuestConfig} from './types'
+import {buildURL} from './helpers/url'
 import xhr from './xhr'
 function axios(config: AxiosReuestConfig): void {
+    processConfig(config);
     xhr(config)
+}
+function processConfig(config: AxiosReuestConfig): void {
+    config.url = transformURL(config);
+}
+function transformURL(config: AxiosReuestConfig): string {
+ const {url, params} = config;
+ return buildURL(url, params);
 }
 export default axios
